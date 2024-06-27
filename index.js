9aa454788528db44cece19c240a7729221c07f05
S-P-Sreelakshmi630 <spsreelakshmi630@gmail.com>
2024-06-27 11:06:55 +0530

    server_running

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 9000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to server at 9000");
-});
+
 
 app.listen(port, (req, res) => {
   console.log("server listening on 9000");
