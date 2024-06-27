7a586166376e1b4d8efdcbea877d131d90a19204
Pavan Vanam <105368989+PAVAN-VANAM@users.noreply.github.com>
2024-06-27 11:10:48 +0530

    add api

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 9000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to server at 9000");
 });
+
+app.get("/api", (req, res) => {
+    res.send("Welcome to Api at 9000");
+})
 
 app.listen(port, (req, res) => {
   console.log("server listening on 9000");
